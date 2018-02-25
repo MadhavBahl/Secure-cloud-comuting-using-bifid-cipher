906594ab8b0026c5ca162d12469b08ee6587a8fe
Madhav Bahl <madhavbahl10@gmail.com>
2018-02-25 16:36:30 +0530

    Make Polybus square

--- playground/tryBifid.js
+++ playground/tryBifid.js
@@ -1,9 +1,9 @@
-var key = "WWOOOWWOAFALSDFNEILQFRNQERF";
+var key = "HelloWorld";
 key = key.toUpperCase();
+key = key.replace(/[^a-zA-Z ]/g, "");
 console.log(key);
 
 var square = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]];
-console.log(square);
 var row = [];
 var count=0, row=-1;
 
@@ -12,11 +12,8 @@ var len = keyArr.length;
 
 // Remove the repetitions from Array
 
-// keyArr.splice(1, 1);
-
-
 for (let i=0;i<len;i++) {
-    if(keyArr[i] == 'J') {
+    if(keyArr[i] == 'J' || keyArr == ' ') {
         keyArr[i] = 'I';
     }
     for (let j=i+1;j<len;j++) {
@@ -38,6 +35,45 @@ for(let i=0; i<len; i++) {
     }
     square[row][count%5] = keyArr[i];
     count++;
+}
+
+// Make an array of the remaining elements
+
+function nextChar(c) {
+    return String.fromCharCode(c.charCodeAt(0) + 1).toUpperCase();
+}
+
+var remArr = [];
+
+for(let i='A';i<='Z';i=nextChar(i)) {
+    let flag = 1;
+    for (let j=0; j<keyArr.length; j++ ) {
+        if (i == keyArr[j] || i == 'J') {
+            flag = 0;
+            j = keyArr.length;
+        }
+
+    }
+    if (flag == 1) {
+        remArr.push(i);
+    }
+}
+
+// Fill the polybus square with remaining elements
+
+var r = Math.floor(count/5);
+var c = count%5;
+var index = 0;
+
+for(let i=r; i<5;i++) {
+    for (let j=0; j<5;j++) {
+        if (square[i][j] != 0) {
+            
+        } else {
+            square[i][j] = remArr[index];
+            index++;
+        }
+    }
 }
 
 console.log(square);
